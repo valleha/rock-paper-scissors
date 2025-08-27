6dbe46583402add6ccb7142ba70a1b07af60c58f
Valentin <hartervalentin@gmail.com>
2025-08-27 10:09:30 +0200

    Remove code for five games
    In order to ad UI to increase user experience remove function to play five games.

--- javascript.js
+++ javascript.js
@@ -16,68 +16,6 @@ function getUserChoice () {
 }
 
 
-function playGame (){
-    
-    let userScore = 0;
-    let computerScore = 0;
-    
-    function playRound(userChoice, computerChoice) {
-        if (userChoice == "rock" && computerChoice == "paper") {
-            computerScore ++; 
-            console.log("You chose rock. Computer choses paper." + 
-                "\nYou lose! Paper beats rock!"); }
-        else if (userChoice == "rock" && computerChoice == "scissors") {
-            userScore ++;
-             console.log("You chose rock. Computer choses scissors." + 
-                "\nYou win! Rock beats scissors!"); }
-        else if (userChoice == "paper" && computerChoice == "scissors") {
-            computerScore ++;
-             console.log("You chose paper. Computer choses scissors." + 
-                "\nYou lose! Scissors beats paper!"); }
-        else if (userChoice == "paper" && computerChoice == "rock") {
-            userScore ++;
-             console.log("You chose paper. Computer choses rock." + 
-                "\nYou win! Paper beats rock!"); }
-        else if (userChoice == "scissors" && computerChoice == "rock") {
-            computerScore ++;
-             console.log("You chose scissors. Computer choses rock." + 
-                "\nYou lose! Rock beats scissors!"); }
-        else if (userChoice == "scissors" && computerChoice == "paper") {
-            userScore ++;
-             console.log("You chose scissors. Computer choses paper." + 
-                "\nYou win! Scissors beats paper!"); }
-        else {
-             console.log("You chose " + userChoice + ". Computer choses " + computerChoice + "." + 
-                "\nIt's a draw!");  }
-    
-    };
-   
-    const firstUserSelection = getUserChoice();
-    const firstComputerSelection = getComputerChoice(); 
-    playRound(firstUserSelection, firstComputerSelection);
-
-    const secondUserSelection = getUserChoice();
-    const secondComputerSelection = getComputerChoice();
-    playRound(secondUserSelection, secondComputerSelection);
-
-    const thirdUserSelection = getUserChoice();
-    const thirdComputerSelection = getComputerChoice();
-    playRound(thirdUserSelection, thirdComputerSelection);
-
-    const fourthUserSelection = getUserChoice();
-    const fourthComputerSelection = getComputerChoice();
-    playRound(fourthUserSelection, fourthComputerSelection);
-
-    const fifthUserSelection = getUserChoice();
-    const fifthComputerSelection = getComputerChoice();
-    playRound(fifthUserSelection, fifthComputerSelection);
-
-    function finalScore () {
-        return "The final score is: human " + userScore + " : computer " + computerScore;   
-    }
-
-    console.log(finalScore());
-}
 
 playGame();
 
