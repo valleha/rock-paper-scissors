93dc47f74cc2197c3afaa67197c5ba244503ec8d
Valentin <hartervalentin@gmail.com>
2025-08-27 22:44:44 +0200

    Add buttons and fix script tag in html
    Add buttons for user to choose from
    Add defer to html script tag because buttons weren't showing.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,37 @@
 
 
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 30);
-    if (choice < 11) {
-        return "rock";}
-        else if (choice < 21) {
-            return "paper"; }
-            else {
-                return "scissors";
-            }
-}
-function getUserChoice () {
-    return prompt("Enter your choice").toLowerCase();
+
+// function getComputerChoice() {
+//     let choice = Math.floor(Math.random() * 30);
+//     if (choice < 11) {
+//         return "rock";}
+//         else if (choice < 21) {
+//             return "paper"; }
+//             else {
+//                 return "scissors";
+//             }
+// }
+// function getUserChoice () {
+//     return prompt("Enter your choice").toLowerCase();
      
-}
+//}
 
+const division = document.querySelector("div");
+const rock = document.createElement("button");
+const paper = document.createElement("button")
+const scissors = document.createElement("button")
+const callToAction = document.createElement("h1");
 
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+callToAction.textContent = "Choose wisely!"
 
-playGame();
+division.appendChild(callToAction);
+division.appendChild(rock);
+division.appendChild(paper);
+division. appendChild(scissors);
+
 
 
 
