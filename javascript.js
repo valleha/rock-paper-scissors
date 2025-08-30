485eae03641811046d94098898d4a36e440aff39
Valentin <hartervalentin@gmail.com>
2025-08-30 22:21:25 +0200

    Move function to play round to eventlistener
    Break up playRound function into smaller parts for each userChoice. Add the function to an individual eventLisener for each user choice.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,48 @@
 
 
 
-// function getComputerChoice() {
-//     let choice = Math.floor(Math.random() * 30);
-//     if (choice < 11) {
-//         return "rock";}
-//         else if (choice < 21) {
-//             return "paper"; }
-//             else {
-//                 return "scissors";
-//             }
-// }
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 30);
+    if (choice < 11) {
+        return "rock";}
+        else if (choice < 21) {
+            return "paper"; }
+            else {
+                return "scissors";
+            }
+}
+
 // function getUserChoice () {
 //     return prompt("Enter your choice").toLowerCase();
-     
-//}
+//      }
+let computerChoice = "";
+let computerScore = 0;
+let userScore = 0;
+
+// function playRound(userChoice, computerChoice) {
+//     if (userChoice == "rock" && computerChoice == "paper") {
+//         computerScore ++; 
+//         console.log("You lose! Paper beats rock!"); }
+//     else if (userChoice == "rock" && computerChoice == "scissors") {
+//         userScore ++;
+//         console.log("You win! rock beats scissors!")}
+//     else if (userChoice == "paper" && computerChoice == "scissors") {
+//         computerScore ++;
+//         console.log("You lose! scissors beats paper!")}
+//     else if (userChoice == "paper" && computerChoice == "rock") {
+//         userScore ++;
+//         console.log("You win! Paper beats rock!")}
+//     else if (userChoice == "scissors" && computerChoice == "rock") {
+//         computerScore ++;
+//         console.log("You lose! rock beats scissors!")}
+//     else if (userChoice == "scissors" && computerChoice == "paper") {
+//         userScore ++;
+//         console.log("You win! scissors beats paper!")}
+//     else {
+//         console.log("It's a draw!");
+
+//     }
+// };
 
 const division = document.querySelector("div");
 const rock = document.createElement("button");
@@ -27,10 +55,54 @@ paper.textContent = "Paper";
 scissors.textContent = "Scissors";
 callToAction.textContent = "Choose wisely!"
 
+rock.addEventListener("click", () => { 
+        computerChoice = "";
+        computerChoice += getComputerChoice();
+        console.log(computerChoice)
+
+        if (computerChoice == "paper") {computerScore ++; 
+            console.log("You lose! Paper beats rock!"); }
+        else if (computerChoice == "scissors") {
+        userScore ++;
+        console.log("You win! rock beats scissors!")}
+        else {console.log("It's a draw!")}
+    }
+                    )
+
+paper.addEventListener("click", () => { 
+        computerChoice = "";
+        computerChoice += getComputerChoice();
+        console.log(computerChoice)
+
+        if (computerChoice == "scissors") {computerScore ++; 
+            console.log("You lose! Scissors beats paper!"); }
+        else if (computerChoice == "rock") {
+        userScore ++;
+        console.log("You win! Paper beats rock!")}
+        else {console.log("It's a draw!")}
+    }
+                    )
+
+scissors.addEventListener("click", () => { 
+        computerChoice = "";
+        computerChoice += getComputerChoice();
+        console.log(computerChoice)
+
+        if (computerChoice == "rock") {computerScore ++; 
+            console.log("You lose! Rock beats scissors!"); }
+        else if (computerChoice == "paper") {
+        userScore ++;
+        console.log("You win! Scissors beats paper!")}
+        else {console.log("It's a draw!")}
+    }
+                    )
+
+
 division.appendChild(callToAction);
 division.appendChild(rock);
 division.appendChild(paper);
 division. appendChild(scissors);
+
 
 
 
